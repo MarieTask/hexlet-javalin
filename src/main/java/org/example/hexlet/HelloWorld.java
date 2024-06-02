fa665ced9357cbf95fbd762d749b33c033e1dbf9
Таскина Мария <taskinamaria@MacBook-Air-Taskina.local>
2024-06-02 20:19:50 +0300

    dynamic routes

--- src/main/java/org/example/hexlet/HelloWorld.java
+++ src/main/java/org/example/hexlet/HelloWorld.java
@@ -7,10 +7,13 @@ public class HelloWorld {
         // Создаем приложение
         var app = Javalin.create(config -> config.bundledPlugins.enableDevLogging());
         // Описываем, что загрузится по адресу /
-        app.get("/hello", ctx -> {
-                    var name = ctx.queryParamAsClass("name", String.class).getOrDefault("World");
-                    ctx.result("Hello, " + name + "!");
-                });
+        // Обратите внимание, что id — это не обязательно число
+        // Название параметров мы выбрали произвольно
+        app.get("/users/{id}/post/{postId}", ctx -> {
+            var id = ctx.pathParam("id");
+            var postId =  ctx.pathParam("postId");
+            ctx.result("User ID: " + id + " Post ID: " + postId);
+        });
 
         app.post("/users", ctx -> ctx.result("POST /users"));
         app.start(7070);

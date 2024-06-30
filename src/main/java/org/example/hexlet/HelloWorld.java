cbdf528752971c0af98efdcc560d9b4dc18059a9
Таскина Мария <taskinamaria@MacBook-Air-Taskina.local>
2024-06-30 22:41:48 +0300

    add index.jte

--- src/main/java/org/example/hexlet/HelloWorld.java
+++ src/main/java/org/example/hexlet/HelloWorld.java
@@ -4,15 +4,11 @@ import io.javalin.Javalin;
 import io.javalin.rendering.template.JavalinJte;
 import org.example.hexlet.data.DataCourses;
 import org.example.hexlet.dto.courses.CoursesPage;
-import org.example.hexlet.model.Course;
-
-import java.util.List;
 
 import static io.javalin.rendering.template.TemplateUtil.model;
 
 public class HelloWorld {
 
-    private static final List<Course> COURSES = DataCourses.getCourses();
     public static void main(String[] args) {
         var app = Javalin.create(config -> {
             config.bundledPlugins.enableDevLogging();
@@ -20,7 +16,7 @@ public class HelloWorld {
         });
 
         app.get("/courses", ctx -> {
-            var courses = COURSES;
+            var courses = DataCourses.getCourses();
             var header = "Курсы по программированию";
             var page = new CoursesPage(courses, header);
             ctx.render("courses/index.jte", model("page", page));
